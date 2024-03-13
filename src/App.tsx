220339757fc2d6f3803fc93321701b32bed8005f
sakaguchi <kzmsan63@gmail.com>
2024-03-13 17:58:36 +0900

    modify

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import './App.css'
 import { Amplify } from 'aws-amplify'
 import { type AuthUser } from "aws-amplify/auth"
 import { type UseAuthenticator } from '@aws-amplify/ui-react-core'
-import { withAuthenticator, Button, Heading } from '@aws-amplify/ui-react'
-import '@aws-amplify/ui-react'
+import { Authenticator, Button, Heading } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
 import awsconfig from './aws-exports'
 Amplify.configure(awsconfig)
 
@@ -14,13 +14,13 @@ type AppProps = {
 
 const App: React.FC<AppProps> = ({ signOut, user }) => {
   return (
-    <>
+    <Authenticator socialProviders={['google']}>
       <div>
         <Heading level={1}>Hello {user?.username}</Heading>
         <Button onClick={signOut}>Sign out</Button>
       </div>
-    </>
+    </Authenticator>
   )
 }
 
-export default withAuthenticator(App)
+export default App
