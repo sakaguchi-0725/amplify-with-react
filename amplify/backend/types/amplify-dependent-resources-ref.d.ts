2ab086a41e5f954a07195712c753e08c3df24441
sakaguchi <kzmsan63@gmail.com>
2024-03-13 16:26:50 +0900

    amplify auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amplifywithreactcd38d8cd": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Amplify } from 'aws-amplify'
+import { type AuthUser } from "aws-amplify/auth"
+import { type UseAuthenticator } from '@aws-amplify/ui-react-core'
+import { withAuthenticator, Button, Heading } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react'
+import awsconfig from './aws-exports'
+Amplify.configure(awsconfig)
 
-function App() {
-  const [count, setCount] = useState(0)
+type AppProps = {
+  signOut?: UseAuthenticator['signOut']
+  user?: AuthUser
+}
 
+const App: React.FC<AppProps> = ({ signOut, user }) => {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Heading level={1}>Hello {user?.username}</Heading>
+        <Button onClick={signOut}>Sign out</Button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
 
-export default App
+export default withAuthenticator(App)
